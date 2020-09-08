ede4359fe0314a22e8804638349ab75f5c0d284f
siddharth85 <69906884+siddharth85@users.noreply.github.com>
2020-09-08 14:58:24 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -20,7 +20,7 @@
 
 
 static int lt=0; static int rt=0; static int up=0; static int dn=0;
-static int j,j1,j2;
+static int j,j1,j2,j3;
 static int ex=0;
 
 
@@ -100,16 +100,16 @@ void diru(int l,int c1,int c2){
 	PORTB =0b00000000;
 	PORTD = ~((int)(pow(2,(7-c1))+0.5));
 	int m1=0;
-	int j1=c2;
+	 j3=c2;
 	for(int d1=0;d1<l;d1++){
-		m1=m1+(int)(pow(2,j1)+0.5);
-		j1--;
-		if(j1<0){
-			j1=7;
+		m1=m1+(int)(pow(2,j3)+0.5);
+		j3--;
+		if(j3<0){
+			j3=7;
 		}
 	}
 	PORTB |= m1;
-	_delay_ms(10);
+	_delay_ms(100);
 }
 
 void cond(){
@@ -193,12 +193,29 @@ int main(void)
 				}
 				
 			}
+			if(l4>1){
+				l4--;
+				diru(l4,column,v1);
+				v1--;
+				if(v1<0){
+					v1=7;
+				}
+				
+				
+				}
 			if(l1==1 && l2>1){
 				row=j2;
 				if(row<=0){
 					row=8;
 				}
 				h=7-column;
+			}
+			if(l1==1 && l4>1){
+				h=7-column;
+				row=j3+2;
+				if(row>8){
+					row=1;
+				}
 			}
 	
 			if(l1!=length_snake){
@@ -268,7 +285,7 @@ int main(void)
 		   
 		  
 			if(l2==1 && l3>1){
-				v=row;
+				v=row-1;
 				column=7-j1+1;
 				if(column >7){
 					column = 0;
@@ -304,6 +321,54 @@ int main(void)
 		cond();
 		
 		if(lt==1){
+			if(l4>1){
+				l4--;
+				diru(l4,column,v1);
+				v1--;
+				if(v1<0){
+					v1=7;
+				}
+				
+				
+			}
+			
+			if(l2>1){
+				l2--;
+				dird(l2,column,v);
+				v++;
+				if(v>7){
+					v=0;
+				}
+				
+			}
+				if(l3==1 && l4>1){
+					h2=7-column;
+					row=(j3+2);
+					if(row>8){
+						row=1;
+					}
+				}
+				if(l3==1 && l2>1){
+					row=j2;
+					if(row<=0){
+						row=8;
+					}
+					h2=7-column;
+				}
+				if(l3!=length_snake){
+					l3++;
+				}
+				
+				dirl (l3,row,h2 );
+				if(l3==length_snake){
+					h2++;
+				}
+				if(h2>7){
+					h2=0;
+				}
+			
+			
+			/*
 			int row =1;
 			if(l3!=length_snake){
 				l3++;
@@ -315,7 +380,8 @@ int main(void)
 			}
 			if(h2>7){
 				h2=0;
-			}
+			}*/
+			
 		}
 		
 		
@@ -348,9 +414,9 @@ int main(void)
 			if(v1<0){
 				v1=7;
 			}*/
-		if(l1!=0){
+		if(l1>1){
 			l1--;
-			row=3;
+		//	row=3;
 			
 			
 			dirr (l1,row,h );
@@ -360,12 +426,31 @@ int main(void)
 			}
 			
 		}
+		if(l3>1){
+			l3--;
+			// row =3;
+			dirl (l3,row,h2 );
+			h2++;
+			if(h2>7){
+				h2=0;
+			}
+			
+		}
 		if(l4==1 && l1>1){
 			
 			v1=row-1;
 			column=7-j-1;
 			if(column < 0){
 				column=7;
+			}
+			
+		}
+		
+		if(l4==1 && l3>1){
+			v1=row-1;
+			column=7-j1+1;
+			if(column >7){
+				column = 0;
 			}
 			
 		}
